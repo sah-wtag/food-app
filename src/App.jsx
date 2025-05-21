4d2fba51ccff9205449b8604f18c7b4f72222f28
Md Sadman Ahmed <164001931+sah-wtag@users.noreply.github.com>
2025-05-22 02:08:06 +0600

    foodlst components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Search from "./components/Search";
+import FoodList from "./components/FoodList";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
 
   return (
     <div className="App">
-      <Search key={food} foodData={foodData} setFoodData={setFoodData} />
-      {foodData.map((food) => (
-        <h1>{food.title}</h1>
-      ))}
+      <Search foodData={foodData} setFoodData={setFoodData} />
+      <FoodList foodData={foodData} />
     </div>
   );
 }
