27bab091b2d3616af99b2281ae5c14f7c6043a5d
Md Sadman Ahmed <164001931+sah-wtag@users.noreply.github.com>
2025-05-22 01:58:36 +0600

    list up the food search result

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { useEffect, useState } from "react";
 import Search from "./components/Search";
 
 function App() {
+  const [foodData, setFoodData] = useState([]);
+
   return (
     <div className="App">
-      <Search />
+      <Search key={food} foodData={foodData} setFoodData={setFoodData} />
+      {foodData.map((food) => (
+        <h1>{food.title}</h1>
+      ))}
     </div>
   );
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "c40c18b30ff44a7b88e34c28b42e07ab";
 
-export default function Search() {
+export default function Search({ foodData, setFoodData }) {
   const [query, setQuery] = useState("pizza");
-
   useEffect(() => {
     async function fetchFood() {
       const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       const data = await res.json();
-      console.log(data);
+      console.log(data.results);
+      setFoodData(data.results);
     }
     fetchFood();
   }, [query]);
