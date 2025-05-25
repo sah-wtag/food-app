fbbb99eb6c6b3da5d8118af1079561e5c7dabb70
Md Sadman Ahmed <164001931+sah-wtag@users.noreply.github.com>
2025-05-26 01:39:01 +0600

    fetch api for recipe using useEffect

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,3 +1,24 @@
+import { useEffect, useState } from "react";
+
 export default function FoodDetails({ foodId }) {
-  return <div>Food Details: {foodId} </div>;
+  const [food, setFood] = useState({});
+  const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
+  const API_KEY = "c40c18b30ff44a7b88e34c28b42e07ab";
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data);
+      setFood(data);
+    }
+    fetchFood();
+  }, [foodId]);
+
+  return (
+    <div>
+      Food Details: {foodId}
+      <h1> {food.title} </h1>
+      <img src={food.image} alt="" />
+    </div>
+  );
 }
